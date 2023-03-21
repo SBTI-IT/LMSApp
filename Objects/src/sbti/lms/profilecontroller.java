6757c0346a0eec5802983fc3d471b6b26de4e14e
Dipolelo Methi <dipolelodips@gmail.com>
2023-03-21 07:13:59 +0200

    add registration page
    design registration page branch

---
+++ Objects/src/sbti/lms/profilecontroller.java
@@ -0,0 +1,46 @@
+package sbti.lms;
+
+
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.B4AClass;
+import anywheresoftware.b4a.BALayout;
+import anywheresoftware.b4a.debug.*;
+
+public class profilecontroller extends B4AClass.ImplB4AClass implements BA.SubDelegator{
+    private static java.util.HashMap<String, java.lang.reflect.Method> htSubs;
+    private void innerInitialize(BA _ba) throws Exception {
+        if (ba == null) {
+            ba = new BA(_ba, this, htSubs, "sbti.lms.profilecontroller");
+            if (htSubs == null) {
+                ba.loadHtSubs(this.getClass());
+                htSubs = ba.htSubs;
+            }
+            
+        }
+        if (BA.isShellModeRuntimeCheck(ba)) 
+			   this.getClass().getMethod("_class_globals", sbti.lms.profilecontroller.class).invoke(this, new Object[] {null});
+        else
+            ba.raiseEvent2(null, true, "class_globals", false);
+    }
+
+ public anywheresoftware.b4a.keywords.Common __c = null;
+public sbti.lms.main _main = null;
+public sbti.lms.index _index = null;
+public sbti.lms.register _register = null;
+public sbti.lms.starter _starter = null;
+public String  _class_globals() throws Exception{
+ //BA.debugLineNum = 1;BA.debugLine="Sub Class_Globals";
+ //BA.debugLineNum = 3;BA.debugLine="End Sub";
+return "";
+}
+public String  _initialize(anywheresoftware.b4a.BA _ba) throws Exception{
+innerInitialize(_ba);
+ //BA.debugLineNum = 6;BA.debugLine="Public Sub Initialize";
+ //BA.debugLineNum = 8;BA.debugLine="End Sub";
+return "";
+}
+public Object callSub(String sub, Object sender, Object[] args) throws Exception {
+BA.senderHolder.set(sender);
+return BA.SubDelegator.SubNotFound;
+}
+}
---
+++ register.bas
@@ -0,0 +1,63 @@
+﻿B4A=true
+Group=Default Group\View
+ModulesStructureVersion=1
+Type=Activity
+Version=12
+@EndOfDesignText@
+#Region  Activity Attributes 
+	#FullScreen: False
+	#IncludeTitle: True
+#End Region
+
+Sub Process_Globals
+	'These global variables will be declared once when the application starts.
+	'These variables can be accessed from all modules.
+
+End Sub
+
+Sub Globals
+	'These global variables will be redeclared each time the activity is created.
+	'These variables can only be accessed from this module.
+
+	Private btnRegister As Button
+	Private txtEmail As EditText
+	Private txtEmpID As EditText
+	Private txtFullname As EditText
+	Private txtGender As EditText
+	Private txtPassword As EditText
+	Private txtRole As EditText
+	
+End Sub
+
+Sub Activity_Create(FirstTime As Boolean)
+	'Do not forget to load the layout file created with the visual designer. For example:
+	Activity.LoadLayout("register")
+	' Hide input line
+	HideInputLine
+End Sub
+
+Sub Activity_Resume
+
+End Sub
+
+Sub Activity_Pause (UserClosed As Boolean)
+
+End Sub
+
+Sub HideInputLine
+	txtEmpID.Color = Colors.Transparent
+	txtFullname.Color = Colors.Transparent
+	txtEmail.Color = Colors.Transparent
+	txtGender.Color = Colors.Transparent
+	txtRole.Color = Colors.Transparent
+	txtPassword.Color = Colors.Transparent
+End Sub
+
+
+Private Sub txtPassword_EnterPressed
+	
+End Sub
+
+Private Sub btnRegister_Click
+	
+End Sub
